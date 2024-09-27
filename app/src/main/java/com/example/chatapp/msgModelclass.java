0432265adf09af37051deacd1a51bc998dc583ad
aasu80 <aasu3140@gmail.com>
2024-09-28 01:55:58 +0530

    Error solved and code updated

---
+++ app/src/main/java/com/example/chatapp/msgModelclass.java
@@ -0,0 +1,45 @@
+
+
+
+package com.example.chatapp;
+
+
+public class msgModelclass {
+    String message;
+    String senderid;
+    long timeStamp;
+
+    //Create Empty Constructor
+    public msgModelclass() {
+    }
+    //Create Constructor
+    public msgModelclass(String message, String senderid, long timeStamp) {
+        this.message = message;
+        this.senderid = senderid;
+        this.timeStamp = timeStamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSenderid() {
+        return senderid;
+    }
+
+    public void setSenderid(String senderid) {
+        this.senderid = senderid;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+}
